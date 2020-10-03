d3e3ed99505e8fadd1fc848cc813465d53b51006
Miguel <miguel.prada@gmail.com>
2020-10-03 18:39:33 +0200

    Tabla de niveles

---
+++ tables.asm
@@ -0,0 +1,16 @@
+' TABLA DE DIRECCIONAMIENTO PARA LOS NIVELES
+
+dim tablelevels (11) as uInteger
+
+tablelevels(0) = @LEVEL001
+tablelevels(1) = @LEVEL002
+tablelevels(2) = @LEVEL003
+tablelevels(3) = @LEVEL004
+tablelevels(4) = @LEVEL005
+tablelevels(5) = @LEVEL006
+tablelevels(6) = @LEVEL007
+tablelevels(7) = @LEVEL008
+tablelevels(8) = @LEVEL009
+tablelevels(9) = @LEVEL010
+tablelevels(10) = @LEVEL011
+tablelevels(11) = @LEVEL012
