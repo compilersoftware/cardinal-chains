a8c2189d48afa3387ef9bdf2713ac98d74b5d7f8
Miguel <miguel.prada@gmail.com>
2020-10-04 11:37:47 +0200

    Añadido hasta nivel 50 y comprobado que se pueden jugar y finalizar

--- numeritos.bas
+++ numeritos.bas
@@ -35,7 +35,7 @@ DIM xBoard, yBoard as uByte
 
 ' NIVEL EN JUEGO Y MÁXIMO NÚMERO DE NIVELES
 DIM level as Integer = 0
-dim levelmax as Integer = 31
+dim levelmax as Integer = 49
 
 ' PARA LA CADENA DEL MARCADOR DE NIVEL
 DIM m$ as string
--- tables.asm
+++ tables.asm
@@ -1,6 +1,6 @@
 ' TABLA DE DIRECCIONAMIENTO PARA LOS NIVELES
 
-dim tablelevels (31) as uInteger
+dim tablelevels (49) as uInteger
 
 tablelevels(0) = @LEVEL001
 tablelevels(1) = @LEVEL002
@@ -34,3 +34,22 @@ tablelevels(28) = @LEVEL029
 tablelevels(29) = @LEVEL030
 tablelevels(30) = @LEVEL031
 tablelevels(31) = @LEVEL032
+
+tablelevels(32) = @LEVEL033
+tablelevels(33) = @LEVEL034
+tablelevels(34) = @LEVEL035
+tablelevels(35) = @LEVEL036
+tablelevels(36) = @LEVEL037
+tablelevels(37) = @LEVEL038
+tablelevels(38) = @LEVEL039
+tablelevels(39) = @LEVEL040
+tablelevels(40) = @LEVEL041
+tablelevels(41) = @LEVEL042
+tablelevels(42) = @LEVEL043
+tablelevels(43) = @LEVEL044
+tablelevels(44) = @LEVEL045
+tablelevels(45) = @LEVEL046
+tablelevels(46) = @LEVEL047
+tablelevels(47) = @LEVEL048
+tablelevels(48) = @LEVEL049
+tablelevels(49) = @LEVEL050
